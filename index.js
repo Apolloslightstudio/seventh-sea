17c2006768c72c614cfa5bcc878f2582d6cb2369
Apolloslightstudio <82015038+Apolloslightstudio@users.noreply.github.com>
2021-04-06 01:30:43 -0500

    Update index.js
    Commenting out original parsing

--- index.js
+++ index.js
@@ -13,55 +13,55 @@ discordClient.on('message', msg => {
   if (msg.author.bot) return;
 
 
-  var linkRegexp = /\[.+\]\(.+\)/gm;
-  var actionsRegexp = /::.*::/gm;
-  var quotesRegexp = /".*"/gm;
+  //var linkRegexp = /\[.+\]\(.+\)/gm;
+  //var actionsRegexp = /::.*::/gm;
+  //var quotesRegexp = /".*"/gm;
 
-  var links = msg.content.match(linkRegexp);
-  var actions = msg.content.match(actionsRegexp);
-  var quotes = msg.content.match(quotesRegexp);
+  //var links = msg.content.match(linkRegexp);
+  //var actions = msg.content.match(actionsRegexp);
+  //var quotes = msg.content.match(quotesRegexp);
 
-  links = links || [];
-  actions = actions || [];
-  quotes = quotes || [];
+  //links = links || [];
+  //actions = actions || [];
+  //quotes = quotes || [];
   
-  if (links.length > 0 || actions.length > 0 || quotes.length > 0) {
-    var embedOptions = {
-      color: '#0099ff',
-      image: {
-        url: msg.author.avatarURL(),
-      },
-      title: msg.author.username,
-      fields: []
-    };
+  //if (links.length > 0 || actions.length > 0 || quotes.length > 0) {
+    //var embedOptions = {
+      //color: '#0099ff',
+      //image: {
+        //url: msg.author.avatarURL(),
+      //},
+      //title: msg.author.username,
+      //fields: []
+    //};
 
-    if (links.length > 0) {
-      embedOptions.fields.push({
-        name: "Rolls",
-        value: links.join('\n')
-      });
-    }
+    //if (links.length > 0) {
+      //embedOptions.fields.push({
+        //name: "Rolls",
+        //value: links.join('\n')
+      //});
+    //}
 
-    if (actions.length > 0) {
-      for (var i = 0; i < actions.length; i++) {
-        embedOptions.fields.push({
-          name: "Action",
-          value: actions[i].slice(0, 1023) // there is a limit on length
-        });
-      }
-    }
+    //if (actions.length > 0) {
+      //for (var i = 0; i < actions.length; i++) {
+        //embedOptions.fields.push({
+          //name: "Action",
+          //value: actions[i].slice(0, 1023) // there is a limit on length
+        //});
+      //}
+    //}
 
-    if (quotes.length > 0) {
-      embedOptions['description'] = quotes.join('\n');
-    }
+    //if (quotes.length > 0) {
+      //embedOptions['description'] = quotes.join('\n');
+    //}
 
     //discordClient.channels.cache.get('CHANNEL ID').send(...)
     //msg.guild.channels.cache.find(i => i.name === 'CHANNEL NAME').send(...)
     
-    msg.channel.send({
-      embed: embedOptions
-    });
-  }
+    //msg.channel.send({
+      //embed: embedOptions
+   //});
+  //}
 });
 
 // this will pull from .env file locally, or ENV from heroku
