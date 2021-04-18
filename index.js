6d57b1b307aa0908dd86c48d1a9fc65f27448eae
Apolloslightstudio <82015038+Apolloslightstudio@users.noreply.github.com>
2021-04-17 20:24:38 -0500

    Update index.js
    Rich Embed?

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ discordClient.on('message', msg => {
 
   
   var eachParagraph = _.split(msg.content, '\n\n');
-  var embed = new Discord.MessageEmbed();
+  var embed = new Discord.RichEmbed();
+  //var embed = new Discord.MessageEmbed();
 
   embed.setAuthor(msg.author.username, msg.author.avatarURL());
   embed.setTitle(msg.channel.parent.name);
